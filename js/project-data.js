9db2cac9c3121cbee618194ae71c7a1f71d05a8c
Ming Li <Mingrkli@gmail.com>
2022-09-13 17:05:04 -0700

    Update

--- js/project-data.js
+++ js/project-data.js
@@ -1,28 +1,8 @@
 projectData = [
     {
-        title: 'Minnew News Site - Suggested by Jake',
-        img: 'img/other-projects/minnew.png',
-        link: 'https://mingrkli-minnew.netlify.app/'
-    },
-    {
-        title: "Jake's Best Day",
-        img: 'img/other-projects/yoi.png',
-        link: 'https://jake-best-day.netlify.app/'
-    },
-    {
-        title: 'Mesco Shopping Site With Working Cart',
-        img: 'img/other-projects/mesco.png',
-        link: 'https://mesco-shopping.netlify.app/'
-    },
-    {
-        title: "Peakalo's Diner - Suggested by Ace",
-        img: 'img/other-projects/diner.png',
-        link: 'https://peakalos-diner.netlify.app/'
-    },
-    {
-        title: 'Game Club - Suggested by Ace',
-        img: 'img/other-projects/wg.png',
-        link: 'https://mingrkli-gameclub.netlify.app/'
+        title: 'Eyeballs Watching',
+        img: 'img/other-projects/eye.png',
+        link: 'https://mingrkli-spooky.netlify.app/'
     },
     {
         title: 'Sound Hearing Animation',
@@ -43,6 +23,26 @@ projectData = [
         title: 'Mouse Character Trail',
         img: 'img/other-projects/ct.png',
         link: 'https://character-trail.netlify.app/'
+    },
+    {
+        title: 'Minnew News Site - Suggested by Jake',
+        img: 'img/other-projects/minnew.png',
+        link: 'https://mingrkli-minnew.netlify.app/'
+    },
+    {
+        title: "Jake's Best Day",
+        img: 'img/other-projects/yoi.png',
+        link: 'https://jake-best-day.netlify.app/'
+    },
+    {
+        title: 'Mesco Shopping Site With Working Cart',
+        img: 'img/other-projects/mesco.png',
+        link: 'https://mesco-shopping.netlify.app/'
+    },
+    {
+        title: "Peakalo's Diner - Suggested by Ace",
+        img: 'img/other-projects/diner.png',
+        link: 'https://peakalos-diner.netlify.app/'
     },
     {
         title: 'Color Time With Local Storage',
